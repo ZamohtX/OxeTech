cbbf29c577b3dcc9ebe36152eab98c5eff3e02d4
Zamoht <thomazx4v13r@gmail.com>
2025-05-24 00:00:38 -0300

    Pega os dados do cnpji por API no serializer

--- financeiro/serializers.py
+++ financeiro/serializers.py
@@ -3,21 +3,54 @@ from financeiro.models import (
     
     LANGUAGE_CHOICES, 
     STYLE_CHOICES,
-    Fornecedor
+    Fornecedor,
+    Cidade
 )
 import requests
 
+class CidadeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cidade
+        fields = ['id', 'nome', 'estado']
+
 class FornecedorSerializer(serializers.ModelSerializer):
-    api_cnpj = serializers.SerializerMethodField()
-
-    def get_api_cnpj(self, obj):
-        dados_cnpj = requests.get(f'https://receitaws.com.br/v1/cnpj/{obj.cnpj}')
-        print(dados_cnpj.json())
-
-        return dados_cnpj.json()
-    
+    cidade = CidadeSerializer(read_only=True)
 
     class Meta:
         model = Fornecedor
         fields = '__all__'
+
+    def validar_cnpj(self, valor):
+   
+        cnpj = ''.join(filter(str.isdigit, valor))
+
+        if len(cnpj) != 14:
+            raise serializers.ValidationError("CNPJ inválido.")
+
+        api_cnpj = requests.get(f'https://www.receitaws.com.br/v1/cnpj/{cnpj}')
+        if api_cnpj.status_code != 200:
+            raise serializers.ValidationError("Erro ao consultar CNPJ")
+        
+        dados = api_cnpj.json()
+        if dados.get("status") == "ERROR":
+            raise serializers.ValidationError(dados.get("message", "Erro ao consultar CNPJ."))
+
+        # Guardando os dados
+        self._dados_cnpj = {
+            "logradouro" : dados.get("logradouro"),
+            "numero" : dados.get("numero"),
+            "bairro" : dados.get("bairro"),
+            "cep" : dados.get("cep"),
+            "complemento" : dados.get("complemento"),
+            "telefone" : dados.get("telefone"),
+            "email" : dados.get("email"),
+            "logradouro" : dados.get("logradouro"),
+            "atividade_principal" : dados.get("atividade_principal")
+        }
+        return cnpj
     
+    def create(self, validated_data):
+        if hasattr(self, "_dados_cnpj"):
+            validated_data.update(self._dados_cnpj)
+
+        return super().create(validated_data)
