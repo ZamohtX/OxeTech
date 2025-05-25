704e9a48f0348292c218001b3a2eba7cf066db28
Zamoht <thomazx4v13r@gmail.com>
2025-05-25 01:26:41 -0300

    Adiciona CRUD em Fornecedores

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -3,4 +3,5 @@ from financeiro import views
 
 urlpatterns = [
     path('fornecedores/', views.fornecedor_list),
+    path('fornecedor/<int:pk>/', views.fornecedor_detail)
 ]
--- financeiro/views.py
+++ financeiro/views.py
@@ -3,19 +3,40 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from financeiro.models import Fornecedor  
 from financeiro.serializers import FornecedorSerializer 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def fornecedor_list(request):
-
     if request.method == 'GET':
         fornecedores = Fornecedor.objects.all()
         serializer = FornecedorSerializer(fornecedores, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = FornecedorSerializer(data=data)
+        serializer = FornecedorSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+@api_view(['GET', 'PUT', 'DELETE'])
+def fornecedor_detail(request, pk):
+    fornecedor = get_object_or_404(Fornecedor, pk=pk)
+
+    if request.method == 'GET':
+        serializer = FornecedorSerializer(fornecedor)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT':
+        serializer = FornecedorSerializer(fornecedor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        fornecedor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
